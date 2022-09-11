d523ca36e1e0876658d9d2a9c984585ba1f27010
Ndumiso M Macebo <nduzamacebo@gmail.com>
2022-09-10 23:55:17 -0700

    Patience, persistence and perspiration make an unbeatable combination for success.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,19 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i <= 58; i++)
 	{
-		putchar(i);
-	}
+	putchar(i);
+
 	if (i != 57)
-	{
-		putchar(',');
-		putchar(' ');
+
+		continue;
+
+	putchar(',');
+	putchar(' ');
 	}
 
 	putchar('\n');
 
-		return (0);
+	return (0);
 }

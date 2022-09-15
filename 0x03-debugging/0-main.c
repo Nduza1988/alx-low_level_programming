c5c713bbbf34d18aa51a43a8b36708d853b473fc
Ndumiso M Macebo <nduzamacebo@gmail.com>
2022-09-15 16:09:32 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,9 +2,7 @@
 
 
 /**
- *
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
  */
 
@@ -12,11 +10,9 @@ int main(void)
 
 {
 	int i;
-		
-		i = 0;
-		
-		positive_or_negative(i);
-		
-		return (0);
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
 
 }

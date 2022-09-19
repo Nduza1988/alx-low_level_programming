5ba9c60c2e297d54fc9504f530fdf7ee9540d6fe
Ndumiso M Macebo <nduzamacebo@gmail.com>
2022-09-19 14:33:49 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include ""main.h"
+
+/**
+ * print_rev - prints a string in reverse, with a new line
+ * @s: the string reverse
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int len = _strlen(s) - 1;
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}

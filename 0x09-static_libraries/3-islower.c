736fa8d97bbefabfb84cb03a4b92344cb0fd593d
Ndumiso M Macebo <nduzamacebo@gmail.com>
2022-10-05 01:27:31 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - write a function that checks lowercase character.
+ *
+ * @c: Is the int that will use for argument of function.
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by new line to stdout.
+ *
+ * @str: the string to be printed out
+ *
+ * Return: the string
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
+

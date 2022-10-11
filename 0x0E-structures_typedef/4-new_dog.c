bc5634b941e11b1d9b6ce1670feeb4b8a6029f49
Ndumiso M Macebo <nduzamacebo@gmail.com>
2022-10-11 15:54:45 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,27 +24,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (n1 = 0; name[n1]; n1++)
 		;
 	n1++;
-	dog->name = malloc(n1 * sizeof(char));
-	if (dog->name == NULL)
+	dog -> name = malloc(n1 * sizeof(char));
+	if (dog -> name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 	for (i = 0; i < n1; i++)
-		dog->name[i] = name[i];
+		dog -> name[i] = name[i];
 
-	dog->age = age;
+	dog -> age = age;
 	for (o1 = 0; owner[o1]; o1++)
 		;
 	o1++;
-	dog->owner = malloc(o1 * sizeof(char));
-	if (dog->owner == NULL)
+	dog -> owner = malloc(o1 * sizeof(char));
+	if (dog -> owner == NULL)
 	{
-		free(dog->name);
+		free(dog -> name);
 		free(dog);
 		return (NULL);
 	}
 	for (i = 0; i < o1; i++)
-		dog->owner[i] = owner[i];
+		dog -> owner[i] = owner[i];
 	return (dog);
 }

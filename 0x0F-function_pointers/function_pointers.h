4f5ed01cb505038150077666e6461c68f66afeaf
Ndumiso M Macebo <nduzamacebo@gmail.com>
2022-10-12 15:47:29 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTION_POINTERS.H_
-#define _FUNCTION_POINTERS.H_
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
 /**
  * Description: A header file that contains prototypes and functions used
@@ -13,4 +13,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
-#endif /*FUNCTION_POINTERS.H*/
+#endif /*_FUNCTION_POINTERS_*/
